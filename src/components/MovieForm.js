c4d59484a892dcf1a083a5124733ddfcd503c778
Ziyunp <ziyun_poh@hotmail.com>
2020-02-29 12:39:44 +0000

    styling MovieForm

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./MovieForm.css";
 
 class MovieForm extends Component {
   constructor(props) {
@@ -25,33 +26,41 @@ class MovieForm extends Component {
   render() {
     const { title, venues, datetime }  = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <button onClick={this.props.closeModal}>close</button>
-        <label>
-          Movie Title
-          <input type="text" value={title} onChange={this.handleChange}/>
-        </label>
-        <label>
-          Venue
-          <select>
-            <option>
-              {venues}
-            </option>
-          </select>
-        </label>
-        <label>
-          Screening Date
-          <div>
-              <DatePicker
-                selected={datetime}
-                onChange={this.handleCalendarChange}
-                showTimeSelect
-                dateFormat="Pp"
-              />
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <button onClick={this.props.closeModal}>close</button>
+          <div className="row">
+            <label>
+              Movie Title
+              <input type="text" value={title} onChange={this.handleChange}/>
+            </label>
           </div>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+          <div className="row">
+            <label>
+              Venue
+              <select>
+                <option>
+                  {venues}
+                </option>
+              </select>
+            </label>
+          </div>
+          <div className="row">
+          <label>
+            Screening Date
+            <div>
+                <DatePicker
+                  selected={datetime}
+                  onChange={this.handleCalendarChange}
+                  showTimeSelect
+                  dateFormat="Pp"
+                />
+            </div>
+          </label>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     )
   }
 }
