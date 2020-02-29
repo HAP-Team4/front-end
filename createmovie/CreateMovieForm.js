65e17ec5cc751828052634e5fb69e023e068a4d9
Ziyunp <ziyun_poh@hotmail.com>
2020-02-29 10:14:36 +0000

    MovieForm

---
+++ createmovie/CreateMovieForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class MovieForm extends Component {
+  render() {
+    return (
+      <form>
+        <label>
+          Movie Title
+        </label>
+      </form>
+    )
+  }
+}
+
